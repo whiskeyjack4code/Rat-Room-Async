7597ad6a9761a66c464e1e35e7e641a52bbd1fc1
whiskeyjack4code <davidmcmhn@gmail.com>
2026-04-14 16:36:13 +0100

    Set up an async runtime and async I\O with a loop for constant listening

--- src/bin/server.rs
+++ src/bin/server.rs
@@ -1,1 +1,15 @@
-fn main() {}
+use tokio::net::TcpListener;
+
+#[tokio::main]
+async fn main() {
+
+    let listener = TcpListener::bind("127.0.0.1:8888").await.expect("Failed to bind");
+
+    println!("Server listening on port 8888");
+
+    loop {
+        let(_socket, addr) = listener.accept().await.expect("Failed to accept");
+        println!("New connection from {}", addr);
+    }
+
+}
