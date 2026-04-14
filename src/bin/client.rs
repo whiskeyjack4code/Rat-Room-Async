897e64daaec6dc3b3744f7d9e15839268c1a58a6
whiskeyjack4code <davidmcmhn@gmail.com>
2026-04-14 17:31:35 +0100

    Client side loop now has one task to read server messages and one loop to send user input.
    Bug: In the write_all() function, .trim_end() will strip the \n (new-line) character. .next_line() waits for a new line character to signal termination.
    
    Bug Code
    writer.write_all(input.trim_end().as_bytes()).await.unwrap();
    
    Fix:
    writer.write_all(input.as_bytes()).await.unwrap();

--- src/bin/client.rs
+++ src/bin/client.rs
@@ -4,17 +4,34 @@ use tokio::net::TcpStream;
 const SOCKET: &str = "127.0.0.1:8888";
 #[tokio::main]
 async fn main() {
-    let mut stream = TcpStream::connect(SOCKET).await.expect("Failed to connect");
+    let stream = TcpStream::connect(SOCKET).await.expect("Failed to connect");
     println!("Connected to {}", SOCKET);
 
+    let (reader, mut writer) = stream.into_split();
+
+    tokio::spawn(async move {
+        let mut server_reader = BufReader::new(reader);
+        let mut line = String::new();
+
+        loop {
+            line.clear();
+
+            match server_reader.read_line(&mut line).await {
+                Ok(0) => break,
+                Ok(_) => print!("{}", line),
+                Err(_) => break,
+            }
+        }
+    });
+
     let stdin = io::stdin();
-    let mut reader = BufReader::new(stdin);
+    let mut stdin_reader = BufReader::new(stdin);
     let mut input = String::new();
 
     loop {
         input.clear();
-        reader.read_line(&mut input).await.unwrap();
-        stream.write_all(input.trim_end().as_bytes()).await.unwrap();
+        stdin_reader.read_line(&mut input).await.unwrap();
+        writer.write_all(input.as_bytes()).await.unwrap();
     }
 
 }
