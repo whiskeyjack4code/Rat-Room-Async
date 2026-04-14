bc9e8a3e522e9e97694fcdacd2c5ecb2bc5bd07b
whiskeyjack4code <davidmcmhn@gmail.com>
2026-04-14 17:03:46 +0100

    Set up the client for a basic connection using async and tcpstream

--- src/bin/client.rs
+++ src/bin/client.rs
@@ -1,3 +1,9 @@
-fn main() {
-    
+use tokio::net::TcpStream;
+
+const SOCKET: &str = "127.0.0.1:8888";
+#[tokio::main]
+async fn main() {
+    let _stream = TcpStream::connect(SOCKET).await.expect("Failed to connect");
+
+    println!("Connected to {}", SOCKET);
 }
