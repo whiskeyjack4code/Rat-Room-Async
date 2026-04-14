426a17dd1d0039d227ebe3a662ee293769422417
whiskeyjack4code <davidmcmhn@gmail.com>
2026-04-14 17:15:16 +0100

    Get user input from STDIN (async) to allow th client to send messages

--- src/bin/client.rs
+++ src/bin/client.rs
@@ -1,9 +1,20 @@
+use tokio::io::{self, AsyncBufReadExt, AsyncWriteExt, BufReader};
 use tokio::net::TcpStream;
 
 const SOCKET: &str = "127.0.0.1:8888";
 #[tokio::main]
 async fn main() {
-    let _stream = TcpStream::connect(SOCKET).await.expect("Failed to connect");
-
+    let mut stream = TcpStream::connect(SOCKET).await.expect("Failed to connect");
     println!("Connected to {}", SOCKET);
+
+    let stdin = io::stdin();
+    let mut reader = BufReader::new(stdin);
+    let mut input = String::new();
+
+    loop {
+        input.clear();
+        reader.read_line(&mut input).await.unwrap();
+        stream.write_all(input.trim_end().as_bytes()).await.unwrap();
+    }
+
 }
